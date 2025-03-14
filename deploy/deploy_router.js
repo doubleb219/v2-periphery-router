db3ede071107a0da72e84219dad2cbe1abb8aa20
doubleb0219 <brownsbob56@gmail.com>
2025-03-13 21:59:50 -0700

    deploy script

--- deploy/deploy_router.js
+++ deploy/deploy_router.js
@@ -1,13 +1,18 @@
 const hre = require("hardhat");
 
 async function main() {
-    const factory = "0x5BA17Ed538E5CcbA111A93D4253E4A8D24f47DC7";
-    const weth = "0x309C92261178fA0CF748A855e90Ae73FDb79EBc7";
+    const factory = "0x31d76cc8196704e48098C62DCc92dA0f965c1626";
+    const weth = "0xD0716d2d12AD8753E1E007B66A62b66Ce58a9783";
 
-    const Router = await hre.ethers.getContractFactory("UniswapV2Router02");
-    const router = await Router.deploy(factory, weth);
+    // const Router = await hre.ethers.getContractFactory("UniswapV2Router02");
+    // const router = await Router.deploy(factory, weth);
 
-    console.log("Router deployed at:", router.address);
+    // console.log("Router deployed at:", router.address);
+    // await new Promise(resolve => setTimeout(resolve, 30000));
+    await hre.run("verify:verify", {
+        address: "0x7dDb023cdaFeeCd594b4D9174f60AB0811BA3491",
+        constructorArguments: [factory, weth]
+    });
 }
 
 main().catch((error) => {
