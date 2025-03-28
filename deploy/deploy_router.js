7703b892e71833b408fcccdf6947956870033518
doubleb0219 <brownsbob56@gmail.com>
2025-03-28 11:38:47 -0700

    change ws address

--- deploy/deploy_router.js
+++ deploy/deploy_router.js
@@ -2,15 +2,15 @@ const hre = require("hardhat");
 
 async function main() {
     const factory = "0x31d76cc8196704e48098C62DCc92dA0f965c1626";
-    const weth = "0xD0716d2d12AD8753E1E007B66A62b66Ce58a9783";
+    const weth = "0x91000870799f537E16cBa085bbf5e14139C3D630";
 
-    // const Router = await hre.ethers.getContractFactory("UniswapV2Router02");
-    // const router = await Router.deploy(factory, weth);
+    const Router = await hre.ethers.getContractFactory("UniswapV2Router02");
+    const router = await Router.deploy(factory, weth);
 
     // console.log("Router deployed at:", router.address);
     // await new Promise(resolve => setTimeout(resolve, 30000));
     await hre.run("verify:verify", {
-        address: "0x7dDb023cdaFeeCd594b4D9174f60AB0811BA3491",
+        address: router.address,
         constructorArguments: [factory, weth]
     });
 }
--- ignition/modules/Router.ts
+++ ignition/modules/Router.ts
@@ -1,7 +1,10 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("uniswapV2Router02", (m) => {
-    const uniswapV2Router02 = m.contract("uniswapV2Router02", ["0x5BA17Ed538E5CcbA111A93D4253E4A8D24f47DC7", "0x309C92261178fA0CF748A855e90Ae73FDb79EBc7"]);
+    const factory = "0x31d76cc8196704e48098C62DCc92dA0f965c1626"
+    const ws = "0x91000870799f537E16cBa085bbf5e14139C3D630"
+    const uniswapV2Router02 = m.contract("uniswapV2Router02",
+        [factory, ws]);
 
     // m.call(UniswapV2ERC20, "_mint", [10000]);
 
